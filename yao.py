ec5b85a4cbea6e6377f536e0232a15281d8a8edc
thien <thien@users.noreply.github.com>
2018-11-08 22:26:04 +0000

    initial commit of logic gate loader

--- yao.py
+++ yao.py
@@ -4,3 +4,33 @@
 
 # << removed >>
 
+def load_json():
+    return None
+
+
+class Gates:
+    def not(self, x):
+        if x is 1:
+            return 0
+        return 1
+       
+    def or(self, x, y):
+        if x == y and x == 0:
+            return 0
+        return 1
+    
+    def and(self, x, y):
+        if x == y and x == 1:
+            return 1
+        return 0
+    
+    def xor(self, x, y):
+        if x == y:
+            return 1
+        return 0
+    
+    def nand(self, x, y):
+        return self.not(self.and(x,y))
+   
+    def xnor(self, x, y):
+        return self.not(self.xor(x,y))
