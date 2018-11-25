8749f8b1fad7d66389d8e30f15edd18142952598
Thien Nguyen <tn518@texel08.doc.ic.ac.uk>
2018-11-25 20:51:55 +0000

    remove debug file

--- gfuck.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-f = Fernet(key)
-token = f.encrypt(b"my deep dark secret")
-print(f.encrypt(b"my deep dark secret"))
-print(f.encrypt(b"my deep dark secret"))
-print(f.encrypt(b"my deep dark secret"))
-print(f.encrypt(b"my deep dark secret"))
-print(f.encrypt(b"my deep dark secret"))
